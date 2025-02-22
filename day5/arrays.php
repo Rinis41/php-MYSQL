e9fabbe2d404b6131062911e561d65df2ac4d244
Rinis41 <86959494+Rinis41@users.noreply.github.com>
2025-02-22 07:37:50 -0800

    day5

---
+++ day5/arrays.php
@@ -0,0 +1,16 @@
+<?php
+    $grade = array(
+        "Math" => "4",
+        "Art" => "5",
+        "History" => "3",
+        "Music" => "5",
+
+    );
+
+         foreach($grade as $subject => $grade){
+            echo "Subject ".$subject . "->  Grade: ".$grade;
+            echo "<br>";
+         }
+ 
+
+?>
