6c4b4e60d3a38b17bd86c065716f6a32e706f51c
Rinis41 <86959494+Rinis41@users.noreply.github.com>
2025-03-08 07:49:02 -0800

    day7

--- day7/connectiondb.php
+++ day7/connectiondb.php
@@ -1,16 +1,23 @@
 <?php
 $host = "localhost";
+$db = "test123";
 $user = "root";
 $pass = "";
 
 
 try{
-    $conn = new PDO("mysql:host=$host;",$user, $pass);
+    $conn = new PDO("mysql:host=$host;dbname=$db",$user, $pass);
+    $sql = "CREATE TABLE users (id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(30) NOT NULL,
+            password VARCHAR(30) NOT NULL)";
 
-    echo "Connected";
+    $conn->exec($sql);
+    
+
+    echo "Table created sucesfully";
 }catch(Exception $e){
 
-    echo "Not connected";
+    echo "Something went wrong!" .$e->getMessage();
 }    
 
 ?>
