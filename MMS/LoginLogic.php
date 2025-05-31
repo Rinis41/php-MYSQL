9ae309f533daa58b6054fa707c79039631144bbd
Rinis41 <86959494+Rinis41@users.noreply.github.com>
2025-05-31 07:44:44 -0700

    mms

---
+++ MMS/LoginLogic.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+
+    include_once('config.php');
+
+    if(isset($_POST['submit'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        if (empty($username) || empty($password)){
+            echo "Please fill in all fields";
+        }
+    }
+?>
